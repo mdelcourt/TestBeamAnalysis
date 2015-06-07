6eadb6685f0d17a9ac1489017afa819d60787850
Some Body <dummy@anything.com>
2015-06-07 16:20:21 +0200

    adding strip pitch variables

--- EdmToNtupleNoMask/test/BeamDiv.C
+++ EdmToNtupleNoMask/test/BeamDiv.C
@@ -11,6 +11,8 @@
 #include "TStyle.h"
 #include "stdio.h"
 
+double pitch_Mod_01 = 90.;
+double pitch_Mod_02 = 80.;
 
 void BeamDiv(){
 
@@ -19,10 +21,10 @@ void BeamDiv(){
  	std::vector<double> *clusters_position_S2= 0;
 	std::vector<double> *clusters_position_S3= 0;
 
-	TH1F *h_div = new TH1F("","; #mum; Events", 100, -2000. , 2000.);
+	TH1F *h_div = new TH1F("","; D[#mum]; Entries", 100, -3000. , 3000.);
 
 
-	TFile *recofile =  TFile::Open("RECO_USC.00000478.0001.A.storageManager.00.0000.root");
+	TFile *recofile =  TFile::Open("/nfs/dust/cms/user/harbali/output/recoFiles/RECO_run_478_120vcth.root");
 	TTree *recotree = (TTree *) recofile->Get("recotree");
 	recotree->SetBranchAddress("clusters_S0", &clusters_position_S0);
 	recotree->SetBranchAddress("clusters_S1", &clusters_position_S1);
@@ -41,15 +43,14 @@ void BeamDiv(){
 
 		if (  cluster_size_S0 == 1 &&  cluster_size_S1 == 1 && cluster_size_S2 == 1 &&  cluster_size_S3 == 1){
 			for(size_t iStrip = 0; iStrip < cluster_size_S0; ++iStrip){
-				
-	if(TMath::Abs(clusters_position_S0->at(0) - clusters_position_S1->at(0)) < 4. && TMath::Abs(clusters_position_S2->at(0) - clusters_position_S3->at(0)) < 4.){
-					h_div->Fill((((clusters_position_S0->at(0)+clusters_position_S1->at(0))/2.)*90.) - (((clusters_position_S2->at(0)+clusters_position_S3->at(0))/2.)*80.) - 6.01015e+02);
-				}
+				//if(TMath::Abs(clusters_position_S0->at(0) - clusters_position_S1->at(0)) < 4. && TMath::Abs(clusters_position_S2->at(0) - clusters_position_S3->at(0)) < 4.){
+				h_div->Fill((((clusters_position_S0->at(0)+clusters_position_S1->at(0))/2.)*pitch_Mod_01) - (((clusters_position_S2->at(0)+clusters_position_S3->at(0))/2.))*pitch_Mod_02);
+				//}
 			}
 		}
 	}
+	h_div->Fit("gaus");
 
-	gStyle->SetOptFit(1011);
+	//gStyle->SetOptFit(1011);
 	h_div->Draw();
-
 }
