1acd232f1c728b59c1a96f936ea6628590e301fd
kskovpen <Kirill.Skovpen@cern.ch>
2015-06-29 15:43:38 +0200

    correcting keys + more debug printout

--- EdmToNtupleNoMask/python/EdmToNtupleNoMask_cfi.py
+++ EdmToNtupleNoMask/python/EdmToNtupleNoMask_cfi.py
@@ -4,8 +4,8 @@ treeMaker = cms.EDAnalyzer("EdmToNtupleNoMask",
    verbosity = cms.untracked.int32(0),
    detIdVec = cms.vint32( 50000,50004,51011,51012 ),
    detNamesVec = cms.vstring( "det0","det1","det2","det3" ),
-   tdcAddress = cms.string("0x030000FF"),
-   hvAddress = cms.string("0x05010000"),
+   tdcAddress = cms.string("0x080000000"),
+   hvAddress = cms.string("0x05000000"),
    dutAngAddress = cms.string("0x04000000"),
    stubAddress = cms.string("0x0B0000FF")
 )
