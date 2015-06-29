c93948c79ae9974b0e898af2158809c79d85a8e3
kskovpen <Kirill.Skovpen@cern.ch>
2015-06-29 12:35:33 +0200

    sync with 2015 format

--- EdmToNtupleNoMask/plugins/EdmToNtuple.cc
+++ EdmToNtupleNoMask/plugins/EdmToNtuple.cc
@@ -4,23 +4,35 @@
 // Class:      EdmToNtupleNoMask
 // 
 /**\class EdmToNtupleNoMask EdmToNtupleNoMask.cc TestBeamAnalysis/TreeMaker/plugins/EdmToNtupleNoMask.cc
- Description: [one line class summary]
+ Description: Main class to process unpacked test beam data 
  Implementation:
      [Notes on implementation]
 */
 //
-// Author:  Ali Harb , Suvankar Roy Chowdhury
+// Author:  Ali Harb, Suvankar Roy Chowdhury, Nicolas Chanon, Kirill Skovpen
 // 
 //
 
 #include "DataFormats/Common/interface/DetSetVector.h"
+#include "DataFormats/Common/interface/DetSetVectorNew.h"
 #include "DataFormats/SiPixelDigi/interface/PixelDigiCollection.h"
+#include "DataFormats/SiPixelCluster/interface/SiPixelCluster.h"
 #include "Phase2TrackerDAQ/SiStripDigi/interface/SiStripCommissioningDigi.h"
+
+#include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerDigi.h"
+#include "DataFormats/Phase2TrackerDigi/interface/Phase2TrackerCommissioningDigi.h"
+#include "DataFormats/FEDRawData/interface/FEDRawDataCollection.h"
+#include "DataFormats/FEDRawData/src/fed_header.h"
+
+#include "EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDBuffer.h"
+#include "EventFilter/Phase2TrackerRawToDigi/interface/Phase2TrackerFEDHeader.h"
 
 #include "TestBeamAnalysis/EdmToNtupleNoMask/plugins/EdmToNtuple.h"
 #include<vector>
 #include<algorithm>
 #include<iomanip>
+
+using namespace Phase2Tracker;
 
 EdmToNtupleNoMask::EdmToNtupleNoMask(const edm::ParameterSet& iConfig) :
   verbosity_(iConfig.getUntrackedParameter<int>("verbosity", 0))
@@ -39,17 +51,21 @@ EdmToNtupleNoMask::EdmToNtupleNoMask(const edm::ParameterSet& iConfig) :
   if(verbosity_)
     for( auto& e: detIdNamemap_ )
       std::cout << e.first << "=" << e.second << std::endl;
+   
   std::string stemp = iConfig.getParameter<std::string>("tdcAddress");
   tdcAdd_= std::stoul(stemp, nullptr, 16);
   stemp = iConfig.getParameter<std::string>("hvAddress");
   hvAdd_ = std::stoul(stemp, nullptr, 16);
   stemp = iConfig.getParameter<std::string>("dutAngAddress");
   DUTangAdd_ = std::stoul(stemp, nullptr, 16);
+  stemp = iConfig.getParameter<std::string>("stubAddress");
+  stubAdd_ = std::stoul(stemp, nullptr, 16);
   
   if(verbosity_) {
     std::cout << "tdcPhase Address=" << tdcAdd_ << std::endl;
     std::cout << "HVsettings Address=" << hvAdd_ << std::endl;
     std::cout << "DUT angle Address=" << DUTangAdd_ << std::endl;
+    std::cout << "stub Address=" << stubAdd_ << std::endl;
   }
 }
 
@@ -67,9 +83,14 @@ void EdmToNtupleNoMask::beginJob()
   tree_->Branch("tdcPhase", &ev.tdcPhase);
   tree_->Branch("hvSettings", &ev.HVsettings);
   tree_->Branch("dutAngle", &ev.DUTangle);
+  tree_->Branch("stubWord", &ev.stubWord);
+  tree_->Branch("condData", &ev.condData);
+  tree_->Branch("glibStatus", &ev.glibStatus);
+  tree_->Branch("cbc1Status", &ev.cbc1Status);
+  tree_->Branch("cbc2Status", &ev.cbc2Status);
   tree_->Branch("dut_channel", "std::map< std::string,std::vector<int> >", &ev.dut_channel);
   tree_->Branch("dut_row", "std::map< std::string,std::vector<int> >", &ev.dut_row);
-  tree_->Branch("dut_adc", "std::map< std::string,std::vector<unsigned short> >", &ev.dut_adc);
+//  tree_->Branch("dut_adc", "std::map< std::string,std::vector<unsigned short> >", &ev.dut_adc);
 }
 
 void EdmToNtupleNoMask::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
@@ -82,43 +103,119 @@ void EdmToNtupleNoMask::analyze(const edm::Event& iEvent, const edm::EventSetup&
   edm::Timestamp itime = iEvent.time();
   evtInfo.time = itime.value();
   evtInfo.unixtime = itime.unixTime();
-  
-  edm::Handle < edm::DetSet<SiStripCommissioningDigi> > conditions;
-  iEvent.getByLabel("SiStripDigiCondDataproducer", "ConditionData",  conditions);
-  if( conditions.isValid() ) {
-    edm::DetSet<SiStripCommissioningDigi>::const_iterator i_detSet;
-    for( edm::DetSet<SiStripCommissioningDigi>::const_iterator i_detSet = conditions->begin(); 
-	 i_detSet != conditions->end(); ++i_detSet){
-      uint32_t key = i_detSet->getKey();
-      int32_t value = i_detSet->getValue();
-      if(key == tdcAdd_) evtInfo.tdcPhase = value;
-      else if(key == hvAdd_) evtInfo.HVsettings = value;
-      else if(key == DUTangAdd_) evtInfo.DUTangle = value;
-    }
-  }
-  else 
-    std::cerr << "SiStripCommissioningDigi not found!!" << std::endl;
-  
-  edm::Handle< edm::DetSetVector<PixelDigi> > input;
-  iEvent.getByLabel( "SiStripDigitestproducer", "ProcessedRaw", input);
-  if( input.isValid() ) {
-    // loop over modules
-    for(edm::DetSetVector<PixelDigi>::const_iterator it = input->begin() ; it != input->end(); ++it)
-      {
-	int detId = it->id;
-	// loop over hits in the module
-	for(edm::DetSet<PixelDigi>::const_iterator hit = it->begin(); hit!=it->end(); hit++ )
+
+   edm::Handle<FEDRawDataCollection> buffers;
+   iEvent.getByLabel("rawDataCollector",buffers);
+   
+   size_t fedIndex;
+   for( fedIndex = Phase2Tracker::FED_ID_MIN; fedIndex < Phase2Tracker::CMS_FED_ID_MAX; ++fedIndex )
+     {
+	const FEDRawData& fed = buffers->FEDData(fedIndex);
+	if(fed.size()!=0)
 	  {
-            evtInfo.dut_channel[detIdNamemap_[detId]].push_back(hit->channel());
-            evtInfo.dut_row[detIdNamemap_[detId]].push_back(hit->row());
-            evtInfo.dut_adc[detIdNamemap_[detId]].push_back(hit->adc());
+	     // construct buffer
+	     Phase2Tracker:: Phase2TrackerFEDBuffer* buffer = 0;
+	     buffer = new Phase2Tracker::Phase2TrackerFEDBuffer(fed.data(),fed.size());
+	     Phase2TrackerFEDHeader tr_header = buffer->trackerHeader();
+	     
+	     if(verbosity_)
+	       {		  
+		  std::cout << " buffer size : " << buffer->bufferSize() << std::endl;
+		  std::cout << " fed id      : " << fedIndex << std::endl;
+		  std::cout << " Version  : " << std::hex << std::setw(2) << (int) tr_header.getDataFormatVersion() << std::endl;
+		  std::cout << " Mode     : " << std::hex << std::setw(2) << tr_header.getDebugMode() << std::endl;
+		  std::cout << " Type     : " << std::hex << std::setw(2) << (int) tr_header.getEventType() << std::endl;
+		  std::cout << " Readout  : " << std::hex << std::setw(2) << tr_header.getReadoutMode() << std::endl;
+		  std::cout << " Condition Data : " << ( tr_header.getConditionData() ? "Present" : "Absent") << "\n";
+		  std::cout << " Data Type      : " << ( tr_header.getDataType() ? "Real" : "Fake" ) << "\n";
+		  std::cout << " Status   : " << std::hex << std::setw(16)<< (int) tr_header.getGlibStatusCode() << std::endl;
+	       }	     
+	     
+	     evtInfo.condData = tr_header.getConditionData();
+	     evtInfo.glibStatus = (int) tr_header.getGlibStatusCode();
+
+	     if(verbosity_)
+	       {		  
+		  std::cout  << " Nr CBC   : " << std::hex << std::setw(16)<< (int) tr_header.getNumberOfCBC() << std::endl;
+		  std::cout << " CBC stat : ";
+		  for(int i=0; i<tr_header.getNumberOfCBC(); i++)
+		    {
+		       std::cout << std::hex << std::setw(2) << (int) tr_header.CBCStatus()[i] << " " << std::endl;
+		    }	     
+	       }	     
+	     
+	     if (tr_header.getNumberOfCBC()==2)
+	       {
+		  evtInfo.cbc1Status = (int) tr_header.CBCStatus()[0];
+		  evtInfo.cbc2Status = (int) tr_header.CBCStatus()[1];
+	       }
+	  }	
+     }   
+   
+   edm::Handle < std::vector<std::vector<Phase2TrackerCommissioningDigi> > > conditions;
+   iEvent.getByLabel("Phase2TrackerCommissioningDigiProducer", "ConditionData",  conditions);
+   if( !conditions.isValid() ) 
+     {
+	std::cout << "condition data not found" << std::endl;
+	exit(1);
+     }   
+
+   std::vector<std::vector<Phase2TrackerCommissioningDigi> >::const_iterator i_detSet;
+   std::vector<Phase2TrackerCommissioningDigi>::const_iterator j_detSet;
+   
+   for(i_detSet = conditions->begin(); i_detSet != conditions->end(); ++i_detSet)
+     {
+	for(j_detSet = i_detSet->begin(); j_detSet != i_detSet->end(); ++j_detSet)
+	  {	
+	     uint32_t key = j_detSet->getKey();
+	     uint32_t value = j_detSet->getValue();
+	     	     
+	     if(key == tdcAdd_) evtInfo.tdcPhase = value;
+	     else if(key == hvAdd_) evtInfo.HVsettings = value;
+	     else if(key == DUTangAdd_) evtInfo.DUTangle = value;
+	     else if(key == stubAdd_) evtInfo.stubWord = value;
 	  }
-      }
-  }
-  else 
-    std::cerr << "PixelDigi not found!!" << std::endl;
-  
-  
+     }
+   
+   edm::Handle< edm::DetSetVector<Phase2TrackerDigi> > input;
+   iEvent.getByLabel( "Phase2TrackerDigiProducer", "Unsparsified", input);
+   
+   if( !input.isValid() )
+     {
+	std::cout << "Phase2TrackerDigiProducer not found" << std::endl;
+	exit(1);	
+     }   
+
+   edm::Handle< edmNew::DetSetVector<SiPixelCluster> > inputCluster;
+   iEvent.getByLabel( "Phase2TrackerDigiProducer", "Sparsified", inputCluster);
+   
+   if( !inputCluster.isValid() )
+     {
+	std::cout << "Phase2TrackerDigiProducer not found" << std::endl;
+	exit(1);	
+     }   
+   
+   for( edmNew::DetSetVector<SiPixelCluster>::const_iterator itc=inputCluster->begin();
+	itc!=inputCluster->end();itc++ )
+     {
+//	uint32_t detid = itc->id();
+     }   
+   
+   edm::DetSetVector<Phase2TrackerDigi>::const_iterator it;
+   // loop over modules   
+   for(it = input->begin() ; it != input->end(); ++it)
+     {
+	int detId = it->id;
+	
+	// loop over hits in the module
+	for(edm::DetSet<Phase2TrackerDigi>::const_iterator hit = it->begin();
+	    hit!=it->end(); hit++ )
+	  {
+	     evtInfo.dut_channel[detIdNamemap_[detId]].push_back(hit->channel());
+	     evtInfo.dut_row[detIdNamemap_[detId]].push_back(hit->row());
+	  }	
+     }
+   
   v_evtInfo_.push_back(evtInfo);
 }
 
